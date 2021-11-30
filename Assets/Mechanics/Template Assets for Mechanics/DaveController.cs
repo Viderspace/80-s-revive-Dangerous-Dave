40c1e24470b70749319138874d525f4fba63cfd2
Jonatan Vider <50073459+Viderspace@users.noreply.github.com>
2021-11-30 03:22:14 +0200

    Added Tilemaps. Walk/Jump mechanics needs tweking.
    After todays class i have started all mechanics from scratch.
    
    Im Trying to immitate the oldschool character movements and physics that the original game had. to get that right, i cant use rigidbody's standard movement functions like addforce / velocity to make dave walk or jump.
    
    Please look at the Scene at "Template Assets for Mechanics" Folder to see the latest progress.

---
+++ Assets/Mechanics/Template Assets for Mechanics/DaveController.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DaveController : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+
+    private float movespeed =0.2f;
+    private float moveDirection;
+    private bool jump;
+
+    public float jumpduration = 0.5f;
+    private float jumptime =  0.5f;
+
+    private float heightTarget;
+    public float smoothTime = 1.3F;
+    private Vector3 velocity = Vector3.zero;
+
+
+    public float jumpHeight = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = transform.localPosition.x + Input.GetAxis("Horizontal") * movespeed;
+        if (Input.GetKeyDown(KeyCode.UpArrow) && jumptime <= 0)
+        {
+            Debug.Log("jump");
+            heightTarget = transform.localPosition.y + jumpHeight;
+            jumptime = jumpduration;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (jumptime > 0) 
+        {
+            jumptime -= Time.deltaTime;
+            var y = Vector3.SmoothDamp(transform.localPosition, new Vector3(
+                moveDirection,
+                heightTarget, 0), ref velocity, smoothTime).y;
+
+            transform.localPosition = new Vector3(moveDirection, y, 0);
+            return;
+        }
+        
+        transform.localPosition = new Vector3( moveDirection, 
+            transform.localPosition.y, 0);
+    }
+
+}
